6878a7ca1b3b9d74c9563514caf87da8440c7607
Tomato308 <hadarshiri8@gmail.com>
2024-09-15 11:20:51 +0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+
+public class Main
+{
+   public static void main(String[] args) 
+   {
+     BankAccount acct1 = new BankAccount(1000, -1000);
+     System.out.println(acct1.getBalance());
+     BankAccount acct2 = new BankAccount(1000, -1000);
+     System.out.println(acct2.getBalance());
+     acct2.transfer(acct1, 500);
+     System.out.println(acct1.getBalance());
+     System.out.println(acct2.getBalance());
+     acct1.transfer(acct2, 1600);
+     System.out.println("money has been tranfered from Bob to Lisa");
+    }
 }
